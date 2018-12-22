5c77d3d15481e83184f10790e84b1b52f521b3ed
Eric <enewcomer23@gmail.com>
2018-12-22 09:24:15 +0000

    Fixing char arrays

--- charArrays.c
+++ charArrays.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 #define MAXLINE 1000
 
-int getline(char line[], int maxline);
+int getline(char line[], int lim);
 void copy(char to[], char from[]);
 
 main()
 {
-   int len;
-   int max;
+   int len, max;
    char line[MAXLINE];
    char longest[MAXLINE];
 
    max = 0;
-   while((len = getline(line, MAXLINE)) > 0)
+   while ((len=getline(line, MAXLINE)) > 0)
    {
       if (len > max) {
          max = len;
@@ -22,7 +21,6 @@ main()
    if (max > 0) {
          printf("%s", longest);
    }
-   return 0;
 }
 
 int getline(char s[], int lim)
@@ -42,7 +40,7 @@ int getline(char s[], int lim)
    return i;
 }
 
-void copy(char to[], from[])
+void copy(char to[], char from[])
 {
    int i;
    i=0;
