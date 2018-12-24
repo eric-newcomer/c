3e8bfd2b961e33d22723e577237c8b560c5ac275
Eric <enewcomer23@gmail.com>
2018-12-24 21:12:49 +0000

    Finish reverse function

--- reverse.c
+++ reverse.c
@@ -1,18 +1,66 @@
 #include <stdio.h>
 #include <string.h>
+#define MAXLINE 1000
 
-int main()
+int mgetline(char line[], int lim);
+void reverse(char line[]);
+
+int main(void)
 {
-   char arr[100];
+   int len;
+   char line[MAXLINE];
    
-   printf("Enter a string to reverse");
-   reverse(arr);
+   while ((len=mgetline(line, MAXLINE)) > 0)
+   {
+      reverse(line);
+      printf("%s", line);
+   }
+
    return 0;
+}
+
+int mgetline(char s[], int lim)
+{
+   int i,c;
+   for(i=0; i<lim-1 && (c=getchar()) != EOF &&c!='\n'; i++)
+   {
+      s[i] = c;
+   }
+   
+   if (c=='\n')
+   {
+      s[i]=c;
+      i++;
+   }
+
+   return i;
 }
 
 void reverse(char arr[])
 {
-   gets(arr);
-   strrev(arr);
-   printf("Reverse: %s\n", arr);
+   int i,j;
+   char temp;
+
+   for (i=0; arr[i]!='\0';i++)
+   {
+      ;      
+   }
+   i--;
+   if (arr[i]=='\n')
+   {
+      i--;
+   }   
+   j=0;
+   while (j<i) 
+   {
+      temp=arr[j];
+      arr[j] = arr[i];
+      arr[i]=temp;
+      i--;
+      j++;
+   }
 }
+
+
+
+
